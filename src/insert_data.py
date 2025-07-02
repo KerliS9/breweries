27ea9c9cf72d38fd46e298d0cea728bae2b9b92f
Kerli <kerlischroeder9@gmail.com>
2025-07-01 21:43:55 -0300

    fix: insert data without a loop

--- src/insert_data.py
+++ src/insert_data.py
@@ -1,18 +1,15 @@
 from utils import get_db_connection, cur_fetchall, delete_data_from_table
+from psycopg2.extras import execute_values 
 
 
 def insert_raw_data(response_data):
-  table_name = 'rw_list_breweries'
-  delete_data_from_table(table_name)
-  insert_query = """
-    INSERT INTO rw_list_breweries (data) VALUES (%s)"
-    """
-
+    table_name = 'rw_list_breweries'
+    delete_data_from_table(table_name)
     try:
         with get_db_connection() as conn:
             with conn.cursor() as cur:
-                for data in response_data:
-                    cur.execute(insert_query, json.dumps(data))
+                raw_data = [(json.dumps(d),) for d in response_data]
+                execute_values("INSERT INTO rw_list_breweries (data) VALUES (%s)", raw_data)
             conn.commit()
             print("Data inserted successfully.")
     except Exception as e:
