0789d01b3fd98e475c08f819e28d8c527f6248a9
Kerli <kerlischroeder9@gmail.com>
2025-07-07 00:59:38 -0300

    fix:unit tests

--- tests/test_process_data.py
+++ tests/test_process_data.py
@@ -28,14 +28,14 @@ def spark_session():
 def test_normalize_and_partition_breweries(spark_session, mocker):
     """Test the complete normalization and partitioning workflow"""
     mock_init_spark = mocker.patch(
-        'src.utils.init_spark', 
+        'src.process_data.init_spark', 
         return_value=spark_session
     )
-    mock_write_delta = mocker.patch("src.process_data.write.write_delta_partitioned")
+    mock_write_delta = mocker.patch("src.process_data.write_delta_partitioned")
 
     test_data = [
-        {"data": '{"id": "1", "name": "Brew1", "country": "US"}', "timestamp_ingestion": "2023-01-01T00:00:00"},
-        {"data": '{"id": "2", "name": "Brew2", "country": "UK"}', "timestamp_ingestion": "2023-01-01T00:00:00"}
+        {"data": '{"id": "5128df48-79fc-4f0f-8b52-d06be54d0cec", "name": "(405) Brewing Co","brewery_type": "micro", "address_1": "1716 Topeka St", "address_2": null, "address_3": null, "city": "Norman", "state_province": "Oklahoma", "postal_code": "73069-8224", "country": "United States", "longitude": -97.46818222, "latitude": 35.25738891, "phone": "4058160490", "website_url": "http://www.405brewing.com", "state": "Oklahoma", "street": "1716 Topeka St"}',  "timestamp_ingestion": "2023-01-01T00:00:00"},
+        {"data": '{ "id": "9c5a66c8-cc13-416f-a5d9-0a769c87d318", "name": "(512) Brewing Co", "brewery_type": "micro", "address_1": "407 Radam Ln Ste F200", "address_2": null, "address_3": null, "city": "Austin", "state_province": "Texas", "postal_code": "78745-1197", "country": "United States", "longitude": null, "latitude": null, "phone": "5129211545", "website_url": "http://www.512brewing.com", "state": "Texas", "street": "407 Radam Ln Ste F200"}', "timestamp_ingestion": "2023-01-01T00:00:00"}
     ]
     test_df = spark_session.createDataFrame(test_data)
 
@@ -54,20 +54,23 @@ def test_normalize_and_partition_breweries(spark_session, mocker):
     mock_init_spark.assert_called_once()
 
     args, kwargs = mock_write_delta.call_args
-    assert kwargs['path'] == '/warehouse/delta/particionado/silver/silver_list_breweries'
+
+    assert kwargs['output_path'] == '/warehouse/silver/silver_list_breweries'
     assert kwargs['partition_column'] == 'country'
 
-    result_df = args[0]
-    assert sorted(result_df.columns) == sorted(['id', 'name', 'country', 'brewery_type', 'timestamp_ingestion'])
+    result_df = kwargs['df']
+
+    assert sorted(result_df.columns) == sorted(['address_1', 'address_2', 'address_3', 'brewery_type', 'city', 'country', 'id', 'name', 'phone', 'postal_code', 'state', 'state_province', 'street', 'timestamp_ingestion', 'website_url'])
     assert result_df.count() == 2
+
 
 def test_empty_data_handling(spark_session, mocker):
     """Test with empty input DataFrame"""
     mock_init_spark = mocker.patch(
-        'src.utils.init_spark', 
+        'src.process_data.init_spark', 
         return_value=spark_session
     )
-    mock_write_delta = mocker.patch("src.process_data.write.write_delta_partitioned")
+    mock_write_delta = mocker.patch("src.process_data.write_delta_partitioned")
 
     test_df = spark_session.createDataFrame([], "data: string, timestamp_ingestion: timestamp")
 
@@ -83,16 +86,18 @@ def test_empty_data_handling(spark_session, mocker):
 
     normalize_and_partition_breweries()
 
-    args, _ = mock_write_delta.call_args
-    assert args[0].count() == 0
+    args, kwargs = mock_write_delta.call_args
+
+    assert kwargs['df'].count() == 0
+
 
 def test_malformed_json_handling(spark_session, mocker, caplog):
     """Test handling of malformed JSON data"""
     mock_init_spark = mocker.patch(
-        'src.utils.init_spark', 
+        'src.process_data.init_spark', 
         return_value=spark_session
     )
-    mock_write_delta = mocker.patch("src.process_data.write.write_delta_partitioned")
+    mock_write_delta = mocker.patch("src.process_data.write_delta_partitioned")
 
     test_data = [
         {"data": '{"id": "1", "name": "Good Brew"}', "timestamp_ingestion": "2023-01-01T00:00:00"},
@@ -112,18 +117,23 @@ def test_malformed_json_handling(spark_session, mocker, caplog):
 
     normalize_and_partition_breweries()
 
-    args, _ = mock_write_delta.call_args
-    assert args[0].count() == 1
+    args, kwargs = mock_write_delta.call_args
+    print('test args', args)
+    print('test kwargs', kwargs)
+    print('test kwargs', kwargs['df'].count())
+    print(caplog.text)
+    assert kwargs['df'].count() == 1
 
     assert "Error parsing JSON" in caplog.text
+
 
 def test_schema_validation(spark_session, mocker):
     """Test that the schema is properly applied"""
     mock_init_spark = mocker.patch(
-        'src.utils.init_spark', 
+        'src.process_data.init_spark', 
         return_value=spark_session
     )
-    mock_write_delta = mocker.patch("src.process_data.elt_utils.write.write_delta_partitioned")
+    mock_write_delta = mocker.patch("src.process_data.write_delta_partitioned")
 
     test_data = [
         {"data": '{"id": "1", "name": "Brew1", "country": "US", "extra_field": "value"}', 
@@ -142,18 +152,20 @@ def test_schema_validation(spark_session, mocker):
     )
 
     normalize_and_partition_breweries()
-    # Verify schema was enforced (extra field dropped)
-    args, _ = mock_write_delta.call_args
-    assert "extra_field" not in args[0].columns
-    assert "id" in args[0].columns
+
+    args, kwargs = mock_write_delta.call_args
+
+    assert "data" not in kwargs['df'].columns
+    assert "id" in kwargs['df'].columns
+
 
 def test_partitioning_logic(spark_session, mocker):
     """Test that partitioning column exists in final DataFrame"""
     mock_init_spark = mocker.patch(
-        'src.utils.init_spark', 
+        'src.process_data.init_spark', 
         return_value=spark_session
     )
-    mock_write_delta = mocker.patch("src.process_data.elt_utils.write.write_delta_partitioned")
+    mock_write_delta = mocker.patch("src.process_data.write_delta_partitioned")
 
     test_data = [
         {"data": '{"id": "1", "name": "Brew1", "country": "US"}', "timestamp_ingestion": "2023-01-01T00:00:00"}
@@ -171,9 +183,7 @@ def test_partitioning_logic(spark_session, mocker):
     )
 
     normalize_and_partition_breweries()
-    assert mock_write_delta.called, "write_delta_partitioned was not called"
 
     args, kwargs = mock_write_delta.call_args
+
     assert kwargs['partition_column'] == 'country'
-    assert 'country' in args[0].columns
-    
